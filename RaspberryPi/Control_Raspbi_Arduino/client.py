8abb6068281908b91ba31aedf0b95b2be0d0d7b5
Nic <ktae.nic@gmail.com>
2018-10-13 01:03:43 +0900

    2018/10/13 - Arduino-Raspi Key Control Implementation Complete

--- RaspberryPi/Control_Raspbi_Arduino/client.py
+++ RaspberryPi/Control_Raspbi_Arduino/client.py
@@ -1,13 +1,19 @@
 from pynput.keyboard import Key, Listener
 from time import sleep
 import sys
+import io
+import base64
 import requests
 import json
+import binascii
+
 delay_time = 0.3
 url = 'http://0.0.0.0:8001/control/'
+val = 'e'
+cnt = 0
 
 def send_server(key):
-    global url
+    global url, val, cnt
     val = 'e'
     if (key == 'a'):
             val = 'LEFT'
@@ -24,8 +30,47 @@ def send_server(key):
         'key': val
     }
 
+    print('\n\n\t\tjson: {}'.format(json.dumps(payload)))
     r = requests.post(url, data=json.dumps(payload))
+    #parseRequest(r)
+    print('\n\n\t\tresponse: {}'.format(r))
+    file_f = open('./image_data/%s_%02.d.jpg' % (val, cnt), 'wb')
+    file_f.write(r.content)
+    file_f.close()
+    cnt += 1
 
+def parseRequest(r):
+    global cnt
+    print('\n\nr: %s\n\n' % r)
+    
+    #data = json.dumps(r, default=set_default)
+    data = json.loads(r.text)
+
+    resultCode = data['resultCode']
+
+    if (resultCode != 200):
+        sys.exit(-1)
+
+    img_str = data['data']
+    #img_data = base64.b64decode(img_str)
+    #img_data = decode_base64(img_str)
+    #fh = open('./image_data/val_%02d.jpg' % cnt, 'wb')
+    #fh.write(img_data.decode('base64'))
+    #fh.close()
+
+    with open('./image_data/val_%02d.jpg' % cnt, 'w') as f:
+            f.write(img_str)
+
+    cnt += 1
+
+def decode_base64(data):
+    lens = len(data)
+    lenx = lens - (lens % 4 if lens % 4 else 4)
+    try:
+        result = base64.decodestring(data[:lenx])
+        return result
+    except base64.binascii.Error as err:
+        pass
 
 def on_press(key):
     if (key.char == 'a' or key.char == 's' or key.char == 'w' or key.char == 'd' or key.char == 'q'):
--- RaspberryPi/Control_Raspbi_Arduino/route.py
+++ RaspberryPi/Control_Raspbi_Arduino/route.py
@@ -1,27 +1,65 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, make_response, send_file
 import json
 import picamera
+import io
+import serial
 from time import sleep, time
 
 app = Flask( __name__ )
+ser = serial.Serial('/dev/ttyACM0', 9600)
 
+cnt = -1
 @app.route('/control/', methods=['POST'])
 def control():
+    global cnt, ser
     if request.method == 'POST':
         print('\n\t\t' + str(request.data, 'utf-8') + '\n')
         d = json.loads(str(request.data, 'utf-8'))
-        print(d['key'])
-        return '200'
-       
+        print(d['key'].encode())
+
+        # TODO: SEND SERIAL SIGNAL HERE
+        ser.write(str.encode(d['key']+'\n'))
+
+        with picamera.PiCamera() as camera:
+            camera.resolution=(256, 256)
+            camera.framerate=60
+            f = 'temp_file.jpg'
+            camera.capture(f, format='jpeg', use_video_port=True)
+            #val_f = open(f, 'r').read()
+            #var = base64.b64encode(str(val_f).encode())
+            #var = base64.encodestring(open( f, 'rb' ).read())
+        #rtn_payload = make_json_response(200, val_f)
+        #rtn_payload.headers['content-type'] = 'application/json'
+        #print('\n\tjson.load: %s\n' % rtn_payload)
+        #return rtn_payload
+        try:
+            cnt += 1
+            return send_file('./temp_file.jpg', attachment_filename='%s_%02d.jpg' % (d['key'], cnt), mimetype='image/png')
+        except Exception as e:
+            print(str(e))
+            return str(e)
+
+def make_json_response(status_code, image_str):
+        #response = jsonify( {
+        #    'resultCode': status_code,
+        #    'data': image_str,
+        #})
+        #response.status_code = status_code
+        
+        response = json.dumps( {
+            'resultCode': status_code,
+            'data': image_str
+        })
+        
+        return make_response(response, 200)
+
 if __name__ == '__main__':
     app.run( host='0.0.0.0', port=8001, debug=True )
 
+#stream = io.BytesIO()
+#with picamera.PiCamera() as camera:
+#        camera.resolution(256, 256)
+#        camera.framerate = 90
+#        sleep(1)
 
-with picamera.PiCamera() as camera:
-        camera.resolution(256, 256)
-        camera.framerate = 90
-        sleep(1)
-
-        start = time()
-        stream = io.BytesIO()
 # TODO: need to implement 1 req: 1 response function HERE
